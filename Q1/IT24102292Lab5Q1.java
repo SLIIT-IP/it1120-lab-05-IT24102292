3c5eaf1b4eb91a56efce75671d9b4e3474cd2571
IT24102292 <IT24102292@my.sliit.lk>
2024-08-28 12:25:50 +0530

    Add files via upload

---
+++ Q1/IT24102292Lab5Q1.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class IT24102292Lab5Q1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first integer: ");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Enter the second integer: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Enter the third integer: ");
+        int num3 = scanner.nextInt();
+
+        int smallest = num1;
+        int largest = num1;
+
+        if (num2 < smallest) {
+            smallest = num2;
+        }
+        if (num2 > largest) {
+            largest = num2;
+        }
+
+        if (num3 < smallest) {
+            smallest = num3;
+        }
+        if (num3 > largest) {
+            largest = num3;
+        }
+
+	System.out.println("User entered numbers are : " + num1 +" "+ num2 +" "+ num3);
+        System.out.println("The smallest number is: " + smallest);
+        System.out.println("The largest number is: " + largest);
+
+        scanner.close();
+    }
+}
